2095897bdd719012769b91c508b1bd7fa5ca6734
Benjamin Laugueux <benjamin@laugueux.org>
2013-05-09 19:43:24 +0200

    Added some unit tests.

--- tests/UAParser/Tests/Result/ResultTest.php
+++ tests/UAParser/Tests/Result/ResultTest.php
@@ -27,15 +27,29 @@ class ResultTest  extends \PHPUnit_Framework_TestCase
                 'major'  => '6',
                 'minor'  => '0',
                 'patch'  => '2',
+                'rendering_engine'  => 'WebKit',
+            ),
+            'operating_system' => array(
+                'family' => 'Mac OS',
+                'major' => '10',
+                'minor' => '8',
+                'patch' => '4',
             )
         ));
 
         $this->assertInstanceOf('UAParser\Result\Result', $result);
         $this->assertInstanceOf('UAParser\Result\ResultInterface', $result);
-    
+
+        $this->assertInstanceOf('UAParser\Result\BrowserResult', $result->getBrowser());
         $this->assertEquals('Safari', $result->getBrowser()->getFamily());
         $this->assertEquals(6, $result->getBrowser()->getMajor());
         $this->assertEquals(0, $result->getBrowser()->getMinor());
         $this->assertEquals(2, $result->getBrowser()->getPatch());
+    
+        $this->assertInstanceOf('UAParser\Result\OperatingSystemResult', $result->getOperatingSystem());
+        $this->assertEquals('Mac OS', $result->getOperatingSystem()->getFamily());
+        $this->assertEquals(10, $result->getOperatingSystem()->getMajor());
+        $this->assertEquals(8, $result->getOperatingSystem()->getMinor());
+        $this->assertEquals(4, $result->getOperatingSystem()->getPatch());
     }
 }
