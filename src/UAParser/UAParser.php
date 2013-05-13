942cf0856969e1ae2cd2e27fcc5ab7c5f3e7c74c
Remy Lemeunier <remy@yzalis.com>
2013-05-13 16:37:56 +0200

    Fixed cs.

--- src/UAParser/UAParser.php
+++ src/UAParser/UAParser.php
@@ -162,8 +162,8 @@ class UAParser implements UAParserInterface
                 if (!isset($matches[3])) { $matches[3] = null; }
 
                 $result['constructor'] = isset($expression['constructor_replacement']) ? str_replace(array('$1', '$2'), array($matches[1], $matches[2]), $expression['constructor_replacement']) : $matches[1];
-                $result['model'] = isset($expression['model_replacement']) ? str_replace(array('$1', '$2'), array($matches[1], $matches[2]), $expression['model_replacement']) : $matches[2];
-                $result['type'] = isset($expression['type_replacement']) ? $expression['type_replacement'] : $matches[3];
+                $result['model']       = isset($expression['model_replacement']) ? str_replace(array('$1', '$2'), array($matches[1], $matches[2]), $expression['model_replacement']) : $matches[2];
+                $result['type']        = isset($expression['type_replacement']) ? $expression['type_replacement'] : $matches[3];
 
                 return $result;
             }
